91344097571300c79ec09f4646df4c09419294f1
lichong <lc@molasync.com>
2015-06-18 09:20:50 +0800

    commit

--- ls/main.cpp
+++ ls/main.cpp
@@ -10,7 +10,14 @@ int main(int argc, char **argv) {
     auto curPath = sys::dir_getCurrentPath();
     auto curDir = sys::dir_createWithPath(curPath);
     auto files = curDir->getSubfiles();
+    std::string dirColor = "\033[134m";
+    std::string fileColor = "\033[133m";
     for (auto iter = files.begin(); iter != files.end(); iter++) {
+        if (iter->isDirectory()) {
+            std::cout << dirColor;
+        } else {
+            std::cout << fileColor;
+        }
         std::cout << iter->getFilename() << "\t";
     }
     std::cout << std::endl;

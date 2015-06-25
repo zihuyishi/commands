789515ad6ab644f4c490ce7f1c0afa3f8ded0754
lichong <lc@molasync.com>
2015-06-25 16:39:52 +0800

    添加xfile工具

---
+++ ls/xfile.cpp
@@ -0,0 +1,68 @@
+//
+// Created by 李冲 on 15/6/25.
+//
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <cstdlib>
+
+void printUsage(const char *filename) {
+    printf("%s filename [sXXX] [aXXX] [rXXX]\n", filename);
+}
+void printError(const char *errorInfo) {
+    if (errorInfo != nullptr) {
+        auto len = strlen(errorInfo);
+        write(STDERR_FILENO, errorInfo, len);
+    }
+}
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        printUsage(argv[0]);
+        return 0;
+    }
+    int fd;
+    fd = open(argv[1], O_RDWR | O_CREAT,
+              S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+    if (fd == -1) {
+        printError("Open file failed");
+        return -1;
+    }
+    off_t offset;
+    size_t len;
+    char *buffer = nullptr;
+    for (auto i = 2; i < argc; i++) {
+        switch(argv[i][0]) {
+            case 'a':
+                len = strlen(argv[i] + 1);
+                len = static_cast<size_t>(write(fd, argv[i] + 1, len));
+                printf("write %ld data to file.\n", (long)len);
+                break;
+            case 's':
+                offset = atol(argv[i] + 1);
+                offset = lseek(fd, offset, SEEK_SET);
+                printf("set offset to %lld.\n", (long long)offset);
+                break;
+            case 'r':
+                len = static_cast<size_t>(atol(argv[i] + 1));
+                buffer = new char[len + 1];
+                len = static_cast<size_t>(read(fd, buffer, len));
+                printf("read %ld data, and data is :%s\n", len, buffer);
+                delete[] buffer;
+                buffer = nullptr;
+                break;
+            default:
+                break;
+        }
+    }
+    auto result = close(fd);
+    if (result == -1) {
+        char buf[1024];
+        sprintf(buf, "Close file failed with error %s", strerror(errno));
+        printError(buf);
+        return -1;
+    }
+    return 0;
+}
+
